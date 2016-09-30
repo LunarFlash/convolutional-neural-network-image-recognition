946962e1b5afa7731b5d8cdb2c4a1be84a89090b
Terry Wang <Terry@administrators-iMac.local>
2016-09-30 13:39:55 -0400

    Initial Commit

---
+++ Neural Network Image Recognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Neural Network Image Recognition
+//
+//  Created by Terry Wang on 9/30/16.
+//  Copyright © 2016 Vento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
